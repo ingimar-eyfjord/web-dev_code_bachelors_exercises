d4edb2c2b4a8a75052f3b1d8140ce786de9140ac
Ingimar <ingimareys93@gmail.com>
2021-05-22 15:31:14 +0200

    added navbar

--- exams/interface/root/index.js
+++ exams/interface/root/index.js
@@ -11,3 +11,43 @@ function getIcons() {
       .catch(console.error.bind(console));
   });
 }
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+function myFunction() {
+  const stick = document.querySelectorAll(".stick");
+
+  const navbar = document.querySelector("nav");
+  const aside = document.querySelector(".aside");
+
+  stick.forEach((e) => {
+    // Get the offset position of the navbar
+    const sticky = e.offsetTop - 75;
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    if (window.pageYOffset >= sticky && vw > 600) {
+      e.classList.add("FilterBar");
+      navbar.classList.add("noShadow");
+      aside.classList.add("noShadow");
+    } else {
+      e.classList.remove("FilterBar");
+      navbar.classList.remove("noShadow");
+      aside.classList.remove("noShadow");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", click);
+
+function click() {
+  const articles = document.querySelectorAll("article");
+
+  articles.forEach((e) => {
+    e.addEventListener("click", loadPage);
+  });
+}
+function loadPage() {
+  window.open("http://127.0.0.1:56922/root/article.html");
+}
